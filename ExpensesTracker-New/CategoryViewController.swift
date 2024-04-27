534b6094c794f338980633944cad086cd7d3905c
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-04-28 00:31:23 +0530

    Categories tableView WIP.

--- ExpensesTracker-New/CategoryViewController.swift
+++ ExpensesTracker-New/CategoryViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class CategoryViewController: UITableViewController {
-
+    
+    var categoryArray = ["Travel", "Food", "Clothing"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //creating a cell to pass at tableview
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        //getting a value for that position from categoryarray
+        let category = categoryArray[indexPath.row]
+        
+        //setting this value to text label of the cell
+        cell.textLabel?.text = category
+        
+        return cell
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected cell: ", categoryArray[indexPath.row])
+        
+        //to add a check mark when selected
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark 
+        }
+        
+        //to deselect a cell after clicking on (makes it normal looking instead of keeping it look selected(grey))
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
