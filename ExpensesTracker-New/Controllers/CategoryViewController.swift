3d83139a13a980130bb007dd260b73b5da49f388
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-05-01 22:27:12 +0530

    Complete CRUD using CoreData and SearchBar.

--- ExpensesTracker-New/Controllers/CategoryViewController.swift
+++ ExpensesTracker-New/Controllers/CategoryViewController.swift
@@ -6,48 +6,33 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
     
     var categoryArray = [Category]()
     
-    //creating an object of UserDefaults interface to access this persistent local data storage
-//    var defaults = UserDefaults.standard
+    //to get directory path of documents directory for this app (inside sandbox), this is where coredata db file will be stored
+    var dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     
-    //to get directory path of documents directory for this app (inside sandbox)
-//        var dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-    var dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Categories.plist")
-    
+    //to use context created in AppDelegate.swift, we have to access UIApplication.shared singleton, then cast its delegate to AppDelegate class, and access viewContext
+    //UIApplication.shared this gets created for when app runs
+    //shared is a singleton object of UIApplication class
+    //UIApplication class gets created at run time of current class
+    //this class is used as it and AppDelegate inherit UIApplicationDelegate
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
         print(dataFilePath)
         
-//        let newCategory = Category()
-//        newCategory.title = "Travel"
-//        categoryArray.append(newCategory)
-//        
-//        let newCategory2 = Category()
-//        newCategory2.title = "Food"
-//        categoryArray.append(newCategory2)
-//        
-//        let newCategory3 = Category()
-//        newCategory3.title = "Grossaries"
-//        categoryArray.append(newCategory3)
-        
-        
-        //to retrive data from plist (userdefaults) file, and use it in the array
-        //for each data type there is a different defaults method, for arrays it is array 
-//        if let categories = defaults.array(forKey: "CategoryListArray") as? [Category] {
-//            categoryArray = categories
-//        }
-        
-        //to get data from plist into categoriesArray 
+        //to get data from plist into categoriesArray
         loadCategories()
     }
-
+    
     //MARK: - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categoryArray.count
@@ -67,44 +52,59 @@ class CategoryViewController: UITableViewController {
         //setting value for tick mark
         cell.accessoryType = category.done ? .checkmark : .none
         //other way of doing the same
-//        if category.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
+        //        if category.done == true {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //            cell.accessoryType = .none
+        //        }
         
         return cell
     }
     
     //MARK: - Tableview Delegate Methods
+    //to mark done on click
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Selected cell: ", categoryArray[indexPath.row])
         
         categoryArray[indexPath.row].done = !categoryArray[indexPath.row].done
         
-//        if categoryArray[indexPath.row].done == false {
-//            categoryArray[indexPath.row].done = true
-//        } else {
-//            categoryArray[indexPath.row].done = false
-//        }
-        
-        //getting rid of below code as when this method gets triggered it will refresh tableView as well subsequently calling 2 tableView datasource methods, by calling relaoddata method
-        //to add a check mark when selected
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-        
-        //saving updated check mark to plist file
+        //saving updated data in db and reload tableView
         saveCategories()
-        
-        //reload tableview cells
-        tableView.reloadData()
         
         //to deselect a cell after clicking on (makes it normal looking instead of keeping it look selected(grey))
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //to update on click
+    //    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        print("Selected cell: ", categoryArray[indexPath.row])
+    //
+    //        //changing title to mark completed
+    //        categoryArray[indexPath.row].title = "Completed"
+    //
+    //        //saving updated data in db and reload tableView
+    //        saveCategories()
+    //
+    //        //to deselect a cell after clicking on (makes it normal looking instead of keeping it look selected(grey))
+    //        tableView.deselectRow(at: indexPath, animated: true)
+    //    }
+    
+    //to delete on click
+    //    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        print("Selected cell: ", categoryArray[indexPath.row])
+    //
+    //        //to delete from context
+    //        context.delete(categoryArray[indexPath.row])
+    //
+    //        //to delete from category array
+    //        categoryArray.remove(at: indexPath.row)
+    //
+    //        //writing context with updated data to db and reloading tableView
+    //        saveCategories()
+    //
+    //        //to deselect a cell after clicking on (makes it normal looking instead of keeping it look selected(grey))
+    //        tableView.deselectRow(at: indexPath, animated: true)
+    //    }
     
     //MARK: - Add New Categories
     @IBAction func addCategoryPressed(_ sender: Any) {
@@ -118,20 +118,18 @@ class CategoryViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Category", style: .default) { action in
             
             //creating a category object to be passed in array
-            var newCategoryN = Category()
+            var newCategoryN = Category(context: self.context)
             newCategoryN.title = textField.text!
+            newCategoryN.done = false
             
-            //what will happen once we pressed this button
+            
             self.categoryArray.append(newCategoryN)
             
             //setting above created array to user defaults local data storage, to the key specified as below
-//            self.defaults.set(self.categoryArray, forKey: "CategoryListArray")
+            //            self.defaults.set(self.categoryArray, forKey: "CategoryListArray")
             
             //to save category array to our(custom) plist
             self.saveCategories()
-            
-            //to refresh data in the tableView
-            self.tableView.reloadData()
         }
         
         //to add textfield to above alert
@@ -149,35 +147,69 @@ class CategoryViewController: UITableViewController {
     
     //MARK: - Model Manipulation Methods
     
-    //used to save data to plist from array
+    //used to save data to coredata
     func saveCategories() {
         
-        //encoding and then writing this array to above created plist file
-        var encoder = PropertyListEncoder()
         do {
-            //encoding
-            let data = try encoder.encode(categoryArray)
-            //writing data to plist file
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding category array \(error)")
+            print("Error saving context, \(error)")
         }
         
+        //to refresh data in the tableView
+        tableView.reloadData()
     }
     
-    func loadCategories() {
-        //getting content of plist file into data variable
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            //preparing decoder to decode data from variable data
-            let decoder = PropertyListDecoder()
-            do {
-                self.categoryArray = try decoder.decode([Category].self, from: data)
-            } catch {
-                print("Error decoding plist data \(error)")
-            }
-            
+    
+    func loadCategories(with request : NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        //using above created request fetching data from
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
         }
+        //to refresh data in the tableView
+        tableView.reloadData()
     }
     
 }
 
+//MARK: - Search Bar Methods
+extension CategoryViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //query
+        var request: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        print(searchBar.text!)
+        //creating a predicate i.e., where condition
+        var predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        //adding predicate to the request i.e., adding where condition to query
+        request.predicate = predicate
+        
+        //to sort, i.e., adding order by to our query
+        //creating a sortDescriptor (create as many u need)
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        //adding this sortDescriptor to the request (below is an array so can add many)
+        request.sortDescriptors = [sortDescriptor]
+        
+        //fetching request and reloading data i.e., running the query and getting O/P
+        loadCategories(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            //relaoding all data as searchBar is empty
+            loadCategories()
+            
+            //to resign search bar as first responder i.e., deselect search bar and close the keyboard
+            //need to call this within a dispacthqueue main async to keep UI unfrozen 
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+    
+}
